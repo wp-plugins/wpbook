18d9dae9062f5602d1e98bda244cc959bdfa8183
johneckman <johneckman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-14 19:09:09 +0000

    tagging 1.4.2
    git-svn-id: https://plugins.svn.wordpress.org/wpbook/tags/1.4.2@193998 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme/config.php
+++ theme/config.php
@@ -2,10 +2,10 @@
 // the facebook client library
 if(!class_exists('FacebookRestClient')) {
   if (version_compare(PHP_VERSION,'5','>=')) {
-    include_once(ABSPATH . 'wp-content/plugins/wpbook/client/facebook.php');
+    include_once(WP_PLUGIN_DIR . '/wpbook/client/facebook.php');
   } else {
-    include_once(ABSPATH . 'wp-content/plugins/wpbook/php4client/facebook.php');
-    include_once(ABSPATH . 'wp-content/plugins/wpbook/php4client/facebookapi_php4_restlib.php');
+    include_once(WP_PLUGIN_DIR . '/wpbook/php4client/facebook.php');
+    include_once(WP_PLUGIN_DIR . '/wpbook/php4client/facebookapi_php4_restlib.php');
   }
 }
 
@@ -17,6 +17,7 @@ if (!empty($wpbookOptions)) {
 		$wpbookAdminOptions[$key] = $option;
 	}
 
+//get options from wordpress settings
 $api_key = $wpbookAdminOptions['fb_api_key'];
 $secret  = $wpbookAdminOptions['fb_secret'];
 $app_url = $wpbookAdminOptions['fb_app_url'];
@@ -28,6 +29,17 @@ $enable_share = $wpbookAdminOptions['enable_share'];
 $links_position = $wpbookAdminOptions['links_position'];
 $enable_external_link = $wpbookAdminOptions['enable_external_link'];
 $enable_profile_link = $wpbookAdminOptions['enable_profile_link'];
+$timestamp_date_format = $wpbookAdminOptions['timestamp_date_format'];
+$timestamp_time_format = $wpbookAdminOptions['timestamp_time_format'];
+$show_date_title = $wpbookAdminOptions['show_date_title'];
+$custom_header = $wpbookAdminOptions['custom_header'];
+$custom_footer = $wpbookAdminOptions['custom_footer'];
+$show_custom_header_footer = $wpbookAdminOptions['show_custom_header_footer'];
+$app_name = get_bloginfo('name');
+$use_gravatar = $wpbookAdminOptions['use_gravatar'];
+$gravatar_rating = $wpbookAdminOptions['gravatar_rating'];
+$gravatar_default = $wpbookAdminOptions['gravatar_default'];
+$show_pages = $wpbookAdminOptions['show_pages'];
 
 
 $facebook = new Facebook($api_key, $secret);
@@ -60,4 +72,128 @@ $facebook->api_client->call_method('facebook.profile.setFBML',
                                           'profile_main' => '<fb:ref handle="recent_posts" />'
                                            )
                                     );
+
+// utility functions after here
+function get_external_post_url($my_permalink){
+  global $app_url;
+  // code to get the url of the orginal post for use in the "show external url view"
+  $permalink_peices = parse_url($my_permalink);
+  //get the app_url and the preceeding slash
+  $permalink_app_url = "/". $app_url; 
+  //remove /appname
+  $external_post_permalink = str_replace_once($permalink_app_url,"",$permalink_peices[path]);
+  //re-write the post url using the site url 
+  $external_site_url_peices = parse_url(get_bloginfo('wpurl'));
+    
+  //break apart the external site address and get just the "site.com" part
+  $external_site_url = $external_site_url_peices[host];
+  $external_post_url = get_bloginfo('siteurl').  $external_post_permalink;
+  
+  //return "app url is " . $app_url; 
+  return $external_post_url; 
+}  
+  
+//write the custom header and footer 
+function custom_header($custom_template_header,$date,$time){
+  $author = get_the_author();
+  $category = get_the_category();
+  $date = get_the_time($date);
+  $time = get_the_time($time);
+  $permalink = '<a href="' . get_permalink() . '">permalink</a>';
+  $posttags = get_the_tags();  
+  if ($posttags) {
+    $tag_count = count($posttags);
+    $i = 0;
+    foreach($posttags as $tags) {
+      $i++;
+      $write_tags .= $tags->name ;
+      if($i<$tag_count){
+        $write_tags .= ', ';
+      }
+    } 
+  }
+  else {$write_tags  = "no tags";}
+  $postcategory = get_the_category();
+  if ($postcategory) {
+    $category_count = count($postcategory);
+    $i = 0;
+    foreach($postcategory as $category) {
+      $i++;
+      $write_category .= $category->name ;
+      if($i<$category_count){
+        $write_category .= ', ';
+      }
+    } 
+  }
+  else {$write_category  = "no categories";}
+  
+  $custom_template_header = str_replace("%author%", "$author", "$custom_template_header");
+  $custom_template_header = str_replace("%category%", "$write_category", "$custom_template_header");
+  $custom_template_header = str_replace("%time%", "$time", "$custom_template_header");
+  $custom_template_header = str_replace("%date%", "$date", "$custom_template_header");
+  $custom_template_header = str_replace("%tags%", "$write_tags", "$custom_template_header");
+  $custom_template_header = str_replace("%permalink%","$permalink","$custom_template_header");
+  
+  return $custom_template_header;
+}  // end function custom_header
+  
+function custom_footer($custom_template_footer,$date,$time){
+  $author = get_the_author();
+  $category = get_the_category();
+  $date = get_the_time($date);
+  $time = get_the_time($time);
+  $permalink ='<a href="' . get_permalink() . '">permalink</a>';
+  $posttags = get_the_tags();
+  if ($posttags) {
+    $tag_count = count($posttags);
+    $i = 0;
+    foreach($posttags as $tags) {
+      $i++;
+      $write_tags .= $tags->name ;
+      if($i<$tag_count){
+        $write_tags .= ', ';
+      }
+    } 
+  }
+  else {$write_tags  = "no tags";}
+  
+  $postcategory = get_the_category();
+  if ($postcategory) {
+    $category_count = count($postcategory);
+    $i = 0;
+    foreach($postcategory as $category) {
+      $i++;
+      $write_category .= $category->name ;
+      if($i<$category_count){
+        $write_category .= ', ';
+      }
+    } 
+  }
+  else {$write_category  = "no categories";}
+  
+  $custom_template_footer = str_replace("%author%", "$author", "$custom_template_footer");
+  $custom_template_footer = str_replace("%category%", "$write_category", "$custom_template_footer");
+  $custom_template_footer = str_replace("%time%", "$time", "$custom_template_footer");
+  $custom_template_footer = str_replace("%date%", "$date", "$custom_template_footer");
+  $custom_template_footer = str_replace("%tags%", "$write_tags", "$custom_template_footer");
+  $custom_template_footer = str_replace("%permalink%","$permalink","$custom_template_footer");
+  
+  return $custom_template_footer;
+} // end custom footer
+  
+// check to see if external post link contains the app name, and if it does, 
+// only replace the first instance 
+function str_replace_once($needle, $replace, $haystack) {
+  // Looks for the first occurence of $needle in $haystack
+  // and replaces it with $replace.
+  $pos = strpos($haystack, $needle);
+  if ($pos === false) {
+    // Nothing found
+    return $haystack;
+  }
+  return substr_replace($haystack, $replace, $pos, strlen($needle));
+}  
 ?>
+
+
+
--- theme/index.php
+++ theme/index.php
@@ -1,11 +1,10 @@
 <?php
-// the facebook client library
-include_once 'config.php';
-	if(is_home() && isset($_GET['is_invite'])) { // this is the invite page
+// set up occurs in the config.php
+include_once(WP_PLUGIN_DIR . '/wpbook/theme/config.php');
+  if(isset($_GET['is_invite'])) { // this is the invite page
 		if(isset($_POST["ids"])) { // this means we've already added some stuff
 			echo "<center>Thank you for inviting ".sizeof($_POST["ids"])
-        ." of your friends on <b><a href=\"http://apps.facebook.com/".$app_url
-        ."/\">".$app_name."</a></b>.<br><br>\n"; 
+        ." of your friends to ". $app_name .". <br><br>\n"; 
 			echo "<h2><a href=\"http://apps.facebook.com/".$app_url
         ."/\">Click here to return to ".$app_name."</a>.</h2></center>"; 
 		} 
@@ -27,33 +26,44 @@ include_once 'config.php';
 			$content = "<fb:name uid=\"".$user
         ."\" firstnameonly=\"true\" shownetwork=\"false\"/> has started using "
         ."<a href=\"http://apps.facebook.com/".$app_url."/\">"
-        . bloginfo('name')."</a> and thought you should try it out!\n"
+        . $app_name ."</a> and thought you should try it out!\n"
         ."<fb:req-choice url=\"".$facebook->get_add_url()
-        ."\" label=\"Add ".bloginfo('name')." to your profile\"/>"; 
+      ."\" label=\"Add ". $app_name ." to your profile\"/>"; 
 	?>
-			<fb:request-form action="http://apps.facebook.com/<?php echo $app_url ?>" 
-        method="post" type="<?php bloginfo('name'); ?>" 
+<fb:fbml>
+<fb:title>Invite Friends</fb:title>
+      <fb:request-form action="http://apps.facebook.com/<?php echo $app_url ?>" 
+        method="post" type="<? echo $app_name; ?>" 
         content="<? echo htmlentities($content); ?>" 
         image="<? echo $app_image; ?>"> 
 			<fb:multi-friend-selector actiontext="Here are your friends who don't 
-        have <? bloginfo('name') ?> yet. Invite whoever you want - it's free!" 
+have <? echo $app_name; ?> yet. Invite all you want - it's free!" 
         exclude_ids="<? echo $friends; ?>" bypass="cancel" />
 			</fb:request-form> 
+</fb:fbml>
 	<?php
 		}
 	} 
 	else {  // this is the regular blog page
-    $receiver_url = get_bloginfo('wpurl') . '/wp-content/plugins/wpbook/theme/default/xd_receiver.html';
+    // if this is the frontpage, but it is not the blog list page
+    if(is_front_page() && (!is_home())) {
+      echo '<!-- this is a static homepage -->'; // how can I redirect to home page here?
+      // what can I do here? if we're not listing pages, the user will have
+      // no way to get to the list of blog posts . . . 
+      // this template by default will load their designated static page
+      // but will also then load a recent posts list underneath that
+    }
+    $receiver_url = WP_PLUGIN_URL . '/wpbook/theme/default/xd_receiver.html';
 	?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" 
   xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
 <title><?php bloginfo('name'); ?> :: Facebook Blog Application</title>
+<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 <?php wp_head(); ?>
-<link rel="stylesheet" href="<?php bloginfo('wpurl'); ?>/wp-content/plugins/wpbook/theme/default/style.css" 
+<link rel="stylesheet" href="<?php echo WP_PLUGIN_URL ?>/wpbook/theme/default/style.css" 
   type="text/css" media="screen" />
 <BASE TARGET="_top">	
 </head>
@@ -67,18 +77,58 @@ include_once 'config.php';
 } else {
 ?>
 <script src="http://static.ak.facebook.com/js/api_lib/v0.4/FeatureLoader.js.php" type="text/javascript"></script>
-
-<?php if($invite_friends == "true"){
-	$invite_link = 'http://apps.facebook.com/' . $app_url 
-  ."/index.php?is_invite=true&fb_force_mode=fbml";
+<div class="wpbook_header">
+  <?php if($invite_friends == "true"){
+	$invite_link = '<a href="http://apps.facebook.com/' . $app_url 
+  .'/index.php?is_invite=true&fb_force_mode=fbml" class="share"> Invite Friends</a>';
   ?>
-<div style="float:right" class="wpbook_invite_button"><a href="<?php echo("$invite_link") ?>" class="share"> Invite Friends </div>				
+		<div style="float:right;"><span class="wpbook_invite_button"><?php echo("$invite_link") ?></span> </div>	
 <?php } ?>
   <?php if($enable_profile_link == "true"){ ?>
-<div><div id="addProfileButton" style="float:right"></div></div>
+<div> <div id="addProfileButton" style="float:right;"></div></div>
   <?php }?>
+
 	<h3><a href="http://apps.facebook.com/<?php echo $app_url; ?>/" 
     target="_top"><?php bloginfo('name'); ?></a></h3>
+	
+</div>
+<?php if(is_page()){ // is a page ?>
+<div id="content">
+				<div class="box_head clearfix"
+				<h3 class="wpbook_box_header"><?php the_title(); ?></a></h3>
+				<?php if (have_posts()) : while (have_posts()) : the_post();
+					the_content();
+				endwhile; else: ?>
+					<p>
+					<?php _e('Sorry, page does not exist.'); ?>
+					</p>
+			<?php endif; ?> 
+</div>	
+	<?php } 
+else{
+if(is_archive()){ //is an archive page  ?>
+<div class="acomment">
+ <?php /* If this is a category archive */ if (is_category()) { ?>
+			<p><b><?php printf( __('You are currently browsing the %1$s archives for the \'%2$s\' category.'), $app_name, single_cat_title('', false) ) ?></b></p>
+
+					
+			<?php /* If this is a yearly archive */ } elseif (is_day()) { ?>
+			<p><b>You are currently browsing the <?php $app_name; ?> archives for the day <?php the_time('l, F jS, Y'); ?>.</b></p>
+			
+			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
+			<p><b>You are currently browsing the <?php $app_name; ?> archives 	for <?php the_time('F, Y'); ?>.</b></p>
+
+      <?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
+			<p><b>You are currently browsing the <?php $app_name; ?> archives  for the year <?php the_time('Y'); ?>.</b></p>
+			
+		 <?php /* If this is a monthly archive */ } elseif (is_search()) { ?>
+			<p><b>You have searched the <?php $app_name; ?> archives for <strong>'<?php echo wp_specialchars($s); ?>'</strong>. </b></p>
+
+			<?php /* If this is a monthly archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
+			<p><b>You are currently browsing the <?php $app_name; ?> archives.</b></p>
+
+			<?php } }?>
+			</div>
 	<div id="content">
 <?php 	
 	have_posts();
@@ -92,61 +142,47 @@ include_once 'config.php';
                          '%title',FALSE,'');
 ?>
 				<?php } //end if single?> 
-				<div class="box_head clearfix" style="padding: 5px 0 0 0;" 
-          id="post-<?php the_ID(); ?>">
-				<h3 style="padding: 1px 6px 0px 8px; border-top: solid 1px #3B5998; 
-          background: #d8dfea;">
-					<a href="<?php the_permalink(); ?>" target="_top">
+				<div class="box_head clearfix" id="post-<?php the_ID(); ?>">
+				<h3 class="wpbook_box_header">
+					<?php if($show_date_title == "true"){the_time($timestamp_date_format); echo(" - ");}?> <a href="<?php the_permalink(); ?>" target="_top">
             <?php the_title(); ?></a></h3>
-<div class="meta"><?php the_time() ?>, <?php the_time('l, F jS, Y') ?></div>
-
-
+			<?php if(($show_custom_header_footer == "header") || ($show_custom_header_footer == "both")){
+        echo( '<div id="custom_header">'.custom_header($custom_header,$timestamp_date_format,$timestamp_time_format) .'</div>');
+      } // end if for showing customer header
+      ?>
 
 <?php if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "top")) { 
   echo '<p>';
-if($enable_share == "true"){?>
-<span class="wpbook_share_button">
-<?php
-  echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
-  echo urlencode(get_the_title());
-  echo '&amp;p[summary]=';
-  echo urlencode(get_the_excerpt());
-  echo '&amp;p[url]=';
-  echo urlencode(get_permalink());
-  echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
-  echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
-
-?>
-</span>
-<?php } 
-if($enable_external_link == "true"){?>
-
-<span class="wpbook_external_post">
-	<?php 
-// code to get the url of the orginal post for use in the "show external url view"
-//get the permalink
-$permalink_peices = parse_url(get_permalink());
-//get the app_url and the preceeding slash
-$permalink_app_url = "/".$app_url;
-//remove /appname
-$external_post_permalink = str_replace($permalink_app_url,"","$permalink_peices[path]");
-//re-write the post url using the site url 
-$external_site_url_peices = parse_url(get_bloginfo('wpurl'));
-
-//break apart the external site address and get just the "site.com" part
-$external_site_url = $external_site_url_peices[host];
-$exteral_post_url = get_bloginfo('wpurl').$external_post_permalink;
-
-//echo external post url
-echo "<a href='$exteral_post_url' title='View this post on the web at $external_site_url'>View post on $external_site_url</a>";  ?>
-</span>
-<?php }
+  if($enable_share == "true"){?>
+    <span class="wpbook_share_button">
+    <?php
+      echo '<a onclick="window.open(\'http://www.facebook.com/sharer.php?s=100&amp;p[title]=';
+      echo urlencode(get_the_title());
+      echo '&amp;p[summary]=';
+      echo urlencode(get_the_excerpt());
+      echo '&amp;p[url]=';
+      echo urlencode(get_permalink());
+      echo "','sharer','toolbar=0,status=0,width=626,height=436'); return false;\""; 
+      echo ' class="share" title="Send this to friends or post it on your profile.">Share This Post</a>';
+    ?>
+  </span>
+<?php } // end if for enable_share
+  if($enable_external_link == "true"){ ?>
+  <span class="wpbook_external_post"><a href="<?php echo get_external_post_url(get_permalink()); ?>" title="View this post outside Facebok at <?php bloginfo('name'); ?>">View post on <?php bloginfo('name'); ?></a></span>
+<?php } // end if for enable external_link
   echo '</p>';
 } ?>
 			
 	<?php the_content(); ?>	
+			
+					<?php // echo custom footer
+					if(($show_custom_header_footer == "footer") || ($show_custom_header_footer == "both")){	
+            echo('<div id="custom_footer">'.custom_header($custom_footer,$timestamp_date_format,$timestamp_time_format) .'</div>');
+          } // endif for header or footer 
+          ?>
 
-<?php if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "bottom")) { 
+					<?php // get share link 
+if(($enable_share == "true" || $enable_external_link == "true") && ($links_position == "bottom")) { 
   echo '<p>';
 if($enable_share == "true"){?>
 <span class="wpbook_share_button">
@@ -164,26 +200,7 @@ if($enable_share == "true"){?>
 </span>
 <?php } 
 if($enable_external_link == "true"){?>
-
-<span class="wpbook_external_post">
-	<?php 
-// code to get the url of the orginal post for use in the "show external url view"
-//get the permalink
-$permalink_peices = parse_url(get_permalink());
-//get the app_url and the preceeding slash
-$permalink_app_url = "/".$app_url;
-//remove /appname
-$external_post_permalink = str_replace($permalink_app_url,"","$permalink_peices[path]");
-//re-write the post url using the site url 
-$external_site_url_peices = parse_url(get_bloginfo('wpurl'));
-
-//break apart the external site address and get just the "site.com" part
-$external_site_url = $external_site_url_peices[host];
-$exteral_post_url = get_bloginfo('wpurl').$external_post_permalink;
-
-//echo external post url
-echo "<a href='$exteral_post_url' title='View this post on the web at $external_site_url'>View post on $external_site_url</a>";  ?>
-</span>
+<span class="wpbook_external_post"><a href="<?php echo get_external_post_url(get_permalink()); ?>" title="View this post outside Facebook at <?php bloginfo('name'); ?>">View post on <?php bloginfo('name'); ?></a></span>
 <?php }
   echo '</p>';
 } ?>
@@ -194,9 +211,21 @@ echo "<a href='$exteral_post_url' title='View this post on the web at $external_
 		endif; // if have posts	
 ?>
 </div>
-<div class="box_head clearfix" style="padding: 5px 0 0 0;">
-				<h3 style="padding: 1px 6px 0px 8px; border-top: solid 1px #3B5998; 
-          background: #d8dfea;">
+<?php 	 
+} //end if blog or arvhive 
+
+if($show_pages == "true"){?>
+<div class="box_head clearfix">
+				<h3 class="wpbook_box_header">
+					<?php _e('Pages'); ?></h3>
+				<ul>
+					    <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
+				</ul>
+</div>
+<?php }?>
+
+<div class="box_head clearfix">
+				<h3 class="wpbook_box_header">
 					<?php _e('Recent Posts'); ?></h3>
       <ul>
 				<?php wp_recent_posts(10); ?>
@@ -231,10 +260,10 @@ FB_RequireFeatures(["CanvasUtil"], function() {
                    });
 </script>	
 </body>
-<?php	 
-}
+<?php
+  } // end john's catch test
 ?>
 </html>
 <?php
-  }
+  } // end if not invite condition 
 ?>
